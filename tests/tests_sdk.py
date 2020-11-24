a8118749a799dcf8360fc8a29da0c21736f1872e
dkarasawa <daniela.karasawa@mercadolivre.com>
2020-11-24 14:10:58 -0300

    add new Payment draft

--- tests/tests_sdk.py
+++ tests/tests_sdk.py
@@ -8,6 +8,12 @@ from mercadopago.core.RequestOptions import RequestOptions
 SDK = Sdk('APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
 requestOptions = RequestOptions(accessToken='APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966')
 
+id = '0000001'
+
 payment = Payment(SDK)
-print(payment.search({}, requestOptions))
+#print(payment.search({}, requestOptions)) #SEARCH LINHAS 15-24 OK!!
+
+#print(payment.findById(id, requestOptions)) #SEARCH LINHAS 26-32
+#print(payment.findById(id, requestOptions)) #SEARCH LINHAS 36-45
+
 
