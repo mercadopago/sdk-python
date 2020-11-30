501f6146d34ff8c452b8c3532faae60031b2a7d8
Victor Godinho <victor.godinho@mercadolivre.com>
2020-11-30 09:14:45 -0300

    added features

--- tests/test_card.py
+++ tests/test_card.py
@@ -1,9 +1,10 @@
 import sys
 sys.path.append('../')
 
+import unittest
+
 from mercadopago import SDK
 from mercadopago.config import RequestOptions
-import unittest
 
 class TestCard(unittest.TestCase):
     sdk = SDK("APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966")
