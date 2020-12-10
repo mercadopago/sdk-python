15587cd50858761605e73da49e7f4d1636315cac
Victor Godinho <victor.godinho@mercadolivre.com>
2020-12-10 16:58:08 -0300

    removed comment

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,8 +8,6 @@ from mercadopago.config.request_options import RequestOptions
 
 class TestUser(unittest.TestCase):
     sdk = mercadopago.SDK(access_token="APP_USR-558881221729581-091712-44fdc612e60e3e638775d8b4003edd51-471763966")
-    #sdk.access_token="abc"
-    sdk.corporation_id="MEU_CORP_ID"
 
     def test_find_user(self):
         self.assertEqual(self.sdk.user().get()["status"], 200)
