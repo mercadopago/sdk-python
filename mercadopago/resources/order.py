afd7023351919b590eb279380b811ee39fa591a6
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2025-02-03 16:40:07 -0300

    endpoint cancel and capture

--- mercadopago/resources/order.py
+++ mercadopago/resources/order.py
@@ -33,3 +33,41 @@ class Order(MPBase):
             raise ValueError("Param order_object must be a Dictionary")
 
         return self._post(uri="/v1/orders", data=order_object, request_options=request_options)
+
+    def cancel(self, order_id, request_options=None):
+        """[Click here for more info](https://www.mercadopago.com.br/developers/en/reference/order/in-person-payments/point/cancel-order/post) # pylint: disable=line-too-long
+
+        Args:
+            order_id (str): Order ID
+            request_options (mercadopago.config.request_options, optional): An instance of
+            RequestOptions can be pass changing or adding custom options to ur REST call.
+            Defaults to None.
+
+        Raises:
+            ValueError: Param order_id must be a string
+
+        Returns:
+            dict: Order cancellation response
+        """
+        if not isinstance(order_id, str):
+            raise ValueError("Param order_id must be a string")
+
+        return self._post(uri="/v1/orders/" + str(order_id) + "/cancel", request_options=request_options)
+
+    def capture(self, order_id, request_options=None):
+        """[Click here for more info](https://www.mercadopago.com.br/developers/pt/reference/order/online/capture-order/post)  # pylint: disable=line-too-long
+        Args:
+            order_id (str): ID of the order to be captured. This value is returned in the response to the Create order request.
+            request_options (mercadopago.config.request_options, optional): An instance of
+            RequestOptions can be pass changing or adding custom options to ur REST call.
+            Defaults to None.
+        Raises:
+            ValueError: Param order_id must be a string
+        Returns:
+            dict: Order ID returned in the response to the request made for its creation.
+        """
+
+        if not isinstance(order_id, str):
+            raise ValueError("Param order_id must be a string")
+
+        return self._post(uri="/v1/orders/" + str(order_id) + "/capture", request_options=request_options)
--- tests/test_order.py
+++ tests/test_order.py
@@ -58,3 +58,68 @@ class TestOrder(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
+
+class TestOrderCancelAndCapture(unittest.TestCase):
+    sdk = mercadopago.SDK(os.environ['ACCESS_TOKEN'])
+
+    def create_card_token(self):
+        card_token_object = {
+            "card_number": "5031433215406351",
+            "security_code": "123",
+            "expiration_year": "2025",
+            "expiration_month": "11",
+            "cardholder": {
+                "name": "APRO"
+            }
+        }
+        card_token_created = self.sdk.card_token().create(card_token_object)
+        if card_token_created.get("status") != 201 or not card_token_created.get("response"):
+            self.fail(f"Failed to create card token: {card_token_created}")
+        return card_token_created["response"]["id"]
+
+    def create_order_canceled_or_captured(self, card_token_id):
+        order_object_cc = {
+            "type": "online",
+            "processing_mode": "automatic",
+            "total_amount": "200.00",
+            "external_reference": "ext_ref_1234",
+            "payer": {
+                "email": "test_1731350184@testuser.com"
+            },
+            "capture_mode": "manual",
+            "transactions": {
+                "payments": [
+                    {
+                        "amount": "200.00",
+                        "payment_method": {
+                            "id": "master",
+                            "type": "credit_card",
+                            "token": card_token_id,
+                            "installments": 1
+                        }
+                    }
+                ]
+            }
+        }
+        order_created = self.sdk.order().create(order_object_cc)
+        if order_created.get("status") != 201 or not order_created.get("response"):
+            self.fail(f"Failed to create order: {order_created}")
+        return order_created["response"]["id"]
+
+    def test_cancel_order(self):
+        card_token_id = self.create_card_token()
+        order_id = self.create_order_canceled_or_captured(card_token_id)
+        order_canceled = self.sdk.order().cancel(order_id)
+        self.assertEqual(order_canceled["status"], 200)
+        self.assertEqual(order_canceled["response"]["status"], "canceled")
+
+    def test_capture_order(self):
+        card_token_id = self.create_card_token()
+        order_id = self.create_order_canceled_or_captured(card_token_id)
+        order_captured = self.sdk.order().capture(order_id)
+        self.assertEqual(order_captured["status"], 200)
+        self.assertEqual(order_captured["response"]["status"], "processed")
+
+
+if __name__ == "__main__":
+    unittest.main()
