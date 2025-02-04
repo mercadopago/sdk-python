20b2267ea9dfcae3c4467c34e5ba846747a6091b
Danielen Cestari Nunes <danielen.cestari@mercadolivre.com>
2025-02-04 17:19:22 -0300

    create endpoints create and update transaction

--- mercadopago/resources/order.py
+++ mercadopago/resources/order.py
@@ -3,33 +3,81 @@
 """
 from mercadopago.core import MPBase
 
-
 class Order(MPBase):
     """
     This class provides the methods to access the API that will allow you to create
     your own order experience on your website.
-
     From basic to advanced configurations, you control the whole experience.
-
     [Click here for more info](https://www.mercadopago.com/developers/en/guides/online-payments/checkout-api/introduction/)  # pylint: disable=line-too-long
     """
 
     def create(self, order_object, request_options=None):
         """[Click here for more info](https://www.mercadopago.com/developers/en/reference/order/online-payments/create/post/)  # pylint: disable=line-too-long
-
         Args:
             order_object (dict): Order to be created
             request_options (mercadopago.config.request_options, optional): An instance of
             RequestOptions can be pass changing or adding custom options to ur REST call.
             Defaults to None.
-
         Raises:
             ValueError: Param order_object must be a Dictionary
-
         Returns:
             dict: Order creation response
         """
         if not isinstance(order_object, dict):
             raise ValueError("Param order_object must be a Dictionary")
 
-        return self._post(uri="/v1/orders", data=order_object, request_options=request_options)
+        response = self._post(uri="/v1/orders", data=order_object, request_options=request_options)
+        if response.get("status") != 201:
+            raise Exception(f"Failed to create order: {response}")
+        return response
+
+    def add_transaction(self, order_id, transaction_object, request_options=None):
+        """[Click here for more info](https://www.mercadopago.com.br/developers/pt/reference/order/online-payments/add-transaction/post)  # pylint: disable=line-too-long
+
+        Args:
+            order_id (str): The ID of the order to which the transaction will be added
+            transaction_object (dict): Transaction to be added
+            request_options (mercadopago.config.request_options, optional): An instance of
+            RequestOptions can be pass changing or adding custom options to ur REST call.
+            Defaults to None.
+
+        Raises:
+            ValueError: Param transaction_object must be a Dictionary
+
+        Returns:
+            dict: Transaction addition response
+        """
+        if not isinstance(transaction_object, dict):
+            raise ValueError("Param transaction_object must be a Dictionary")
+
+        response = self._post(uri=f"/v1/orders/{order_id}/transactions", data=transaction_object,
+                              request_options=request_options)
+        if response.get("status") != 201:
+            raise Exception(f"Failed to add transaction: {response}")
+        return response
+
+    def update_transaction(self, order_id, transaction_id, transaction_object, request_options=None):
+        """[Click here for more info](https://www.mercadopago.com.br/developers/pt/reference/order/online-payments/update-transaction/put)  # pylint: disable=line-too-long
+
+        Args:
+            order_id (str): The ID of the order to which the transaction belongs
+            transaction_id (str): The ID of the transaction to be updated
+            transaction_object (dict): Transaction details to be updated
+            request_options (mercadopago.config.request_options, optional): An instance of
+            RequestOptions can be pass changing or adding custom options to ur REST call.
+            Defaults to None.
+
+        Raises:
+            ValueError: Param transaction_object must be a Dictionary
+
+        Returns:
+            dict: Transaction update response
+        """
+        if not isinstance(transaction_object, dict):
+            raise ValueError("Param transaction_object must be a Dictionary")
+
+        response = self._put(uri=f"/v1/orders/{order_id}/transactions/{transaction_id}", data=transaction_object,
+                             request_options=request_options)
+        if response.get("status") != 200:
+            raise Exception(f"Failed to update transaction: {response}")
+        return response
--- tests/test_order.py
+++ tests/test_order.py
@@ -56,5 +56,107 @@ class TestOrder(unittest.TestCase):
         self.assertEqual(order_created["status"], 201)
         self.assertEqual(order_created["response"]["status"], "processed")
 
+
+class TestsTransaction(unittest.TestCase):
+    sdk = mercadopago.SDK(os.environ['ACCESS_TOKEN'])
+
+    def create_card_token(self):
+        card_token_object = {
+            "card_number": "5031433215406351",
+            "security_code": "123",
+            "expiration_year": "2025",
+            "expiration_month": "11",
+            "cardholder": {
+                "name": "APRO"
+            }
+        }
+        print("Creating card token...")
+        card_token_created = self.sdk.card_token().create(card_token_object)
+        print("Card token creation response:", card_token_created)
+
+        if card_token_created.get("status") != 201 or not card_token_created.get("response"):
+            self.fail(f"Failed to create card token: {card_token_created}")
+        return card_token_created["response"]["id"]
+
+    def create_order_builder_mode(self, card_token_id):
+        random_email_id = random.randint(100000, 999999)
+        order_object_cc = {
+            "type": "online",
+            "processing_mode": "manual",
+            "total_amount": "200.00",
+            "external_reference": "ext_ref_1234",
+            "payer": {
+                "email": f"test_payer_{random_email_id}@testuser.com"
+            },
+        }
+        print("Creating order in builder mode...")
+        order_created = self.sdk.order().create(order_object_cc)
+        print("Order creation response:", order_created)
+
+        if order_created.get("status") != 201 or not order_created.get("response"):
+            self.fail(f"Failed to create order: {order_created}")
+        return order_created["response"]["id"]
+
+    def test_add_transaction(self):
+        card_token_id = self.create_card_token()
+        print("Card token ID:", card_token_id)
+
+        order_id = self.create_order_builder_mode(card_token_id)
+        print("Order ID:", order_id)
+
+        transaction_object = {
+            "payments": [
+                {
+                    "amount": "1000.00",
+                    "payment_method": {
+                        "id": "master",
+                        "type": "credit_card",
+                        "token": card_token_id,
+                        "installments": 12
+                    }
+                }
+            ]
+        }
+
+        print("Adding transaction...")
+        transaction_added = self.sdk.order().add_transaction(order_id, transaction_object)
+        print("Transaction addition response:", transaction_added)
+
+        self.assertEqual(transaction_added["status"], 201)
+        self.assertEqual(transaction_added["response"]["status"], "processed")
+
+    def test_cancel_transaction(self):
+        card_token_id = self.create_card_token()
+        order_id = self.create_order_builder_mode(card_token_id)
+
+        transaction_object = {
+            "payments": [
+                {
+                    "amount": "1000.00",
+                    "payment_method": {
+                        "id": "master",
+                        "type": "credit_card",
+                        "token": card_token_id,
+                        "installments": 12
+                    }
+                }
+            ]
+        }
+
+        print("Adding transaction for cancellation...")
+        transaction_added = self.sdk.order().add_transaction(order_id, transaction_object)
+        print("Transaction addition response:", transaction_added)
+
+        transaction_id = transaction_added["response"]["id"]
+        print("Transaction ID:", transaction_id)
+
+        print("Cancelling transaction...")
+        transaction_canceled = self.sdk.order().cancel_transaction(order_id, transaction_id)
+        print("Transaction cancellation response:", transaction_canceled)
+
+        self.assertEqual(transaction_canceled["status"], 200)
+        self.assertEqual(transaction_canceled["response"]["status"], "canceled")
+
+
 if __name__ == "__main__":
     unittest.main()
