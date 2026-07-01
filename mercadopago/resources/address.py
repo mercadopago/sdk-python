e45534f46ff83be5758fd51cd9c0810ecfd7b96f
Diego Barajas <diego.barajassuarez@mercadolibre.com.co>
2026-06-30 21:37:33 -0500

    auto-gen: edfcaefa-5ddb-4759-a609-ce72f91f9178 mercadopago/resources/address.py

---
+++ mercadopago/resources/address.py
@@ -0,0 +1,88 @@
+"""Address resource model for MercadoPago SDK."""
+
+from typing import Optional
+from mercadopago.resources.base import Resource
+
+
+class Address(Resource):
+    """Address model representation.
+    
+    Attributes:
+        city: City name
+        country: Country code or name
+        state: State or province
+        street_name: Street name
+        street_number: Street number
+        zip_code: Postal/ZIP code
+    """
+    
+    def __init__(
+        self,
+        city: Optional[str] = None,
+        country: Optional[str] = None,
+        state: Optional[str] = None,
+        street_name: Optional[str] = None,
+        street_number: Optional[str] = None,
+        zip_code: Optional[str] = None,
+        **kwargs
+    ):
+        """Initialize Address instance.
+        
+        Args:
+            city: City name
+            country: Country code or name
+            state: State or province
+            street_name: Street name
+            street_number: Street number
+            zip_code: Postal/ZIP code
+            **kwargs: Additional attributes
+        """
+        super().__init__(**kwargs)
+        self.city = city
+        self.country = country
+        self.state = state
+        self.street_name = street_name
+        self.street_number = street_number
+        self.zip_code = zip_code
+    
+    def to_dict(self) -> dict:
+        """Convert Address instance to dictionary.
+        
+        Returns:
+            Dictionary representation of the address
+        """
+        data = {}
+        
+        if self.city is not None:
+            data['city'] = self.city
+        if self.country is not None:
+            data['country'] = self.country
+        if self.state is not None:
+            data['state'] = self.state
+        if self.street_name is not None:
+            data['street_name'] = self.street_name
+        if self.street_number is not None:
+            data['street_number'] = self.street_number
+        if self.zip_code is not None:
+            data['zip_code'] = self.zip_code
+        
+        return data
+    
+    @classmethod
+    def from_dict(cls, data: dict) -> 'Address':
+        """Create Address instance from dictionary.
+        
+        Args:
+            data: Dictionary containing address data
+            
+        Returns:
+            Address instance
+        """
+        return cls(
+            city=data.get('city'),
+            country=data.get('country'),
+            state=data.get('state'),
+            street_name=data.get('street_name'),
+            street_number=data.get('street_number'),
+            zip_code=data.get('zip_code')
+        )
