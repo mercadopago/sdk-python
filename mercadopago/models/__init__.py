d0af380c85360f2bbc81ceacb66ce75e8517b73f
Diego Barajas <diego.barajassuarez@mercadolibre.com.co>
2026-06-30 21:49:10 -0500

    auto-gen: 464fdbe8-cc5e-40de-b7b9-29ee8d4e1489 mercadopago/models/__init__.py

---
+++ mercadopago/models/__init__.py
@@ -0,0 +1,11 @@
+"""
+MercadoPago SDK Models
+
+This module contains all the data models used by the MercadoPago SDK.
+"""
+
+from .payment_payer import PaymentPayer
+
+__all__ = [
+    'PaymentPayer',
+]
