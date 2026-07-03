588ae200b7a70f89e49d24671b6bcdd7ca65cb5f
Diego Barajas <diego.barajassuarez@mercadolibre.com.co>
2026-07-03 11:15:54 -0500

    auto-gen: spec-agent mercadopago/schemas/__init__.py

---
+++ mercadopago/schemas/__init__.py
@@ -0,0 +1,11 @@
+"""MercadoPago API schema models.
+
+Exposes dataclass models used to build structured request payloads for the
+MercadoPago API.
+"""
+from mercadopago.schemas.address import Address
+
+
+__all__ = (
+    'Address',
+)
